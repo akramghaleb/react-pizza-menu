bcc1ff838eac86f4862a1f30ad1eaac45e212fdb
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-07-17 13:22:12 +0300

    Setting Classes and Text Conditionally

--- src/index.js
+++ src/index.js
@@ -94,27 +94,13 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-
-  if (pizzaObj.soldOut) {
-    return (
-      <li className='pizza sold-out'>
-        <img src={pizzaObj.photoName} alt={pizzaObj.name} />
-        <div>
-          <h3>{pizzaObj.name}</h3>
-          <p>{pizzaObj.ingredients}</p>
-          <span>Sold Out</span>
-        </div>
-      </li>
-    )
-  }
-
   return (
-    <li className='pizza'>
+    <li className={`pizza ${pizzaObj.soldOut && 'sold-out'}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price + 3}$</span>
+        <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price + 3}</span>
       </div>
     </li>
   )
